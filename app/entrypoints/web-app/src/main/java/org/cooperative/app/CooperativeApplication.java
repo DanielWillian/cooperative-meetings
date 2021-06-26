d7ccf42de4e77cad1aabe87121450cc6c40cc71f
Daniel Willian <d.willian_sc@hotmail.com>
2021-06-25 23:31:22 -0300

    Add dependencies and config to spring entrypoint

--- app/entrypoints/web-app/src/main/java/org/cooperative/app/CooperativeApplication.java
+++ app/entrypoints/web-app/src/main/java/org/cooperative/app/CooperativeApplication.java
@@ -2,10 +2,14 @@ package org.cooperative.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"org.cooperative"})
 @ComponentScan(basePackages = {"org.cooperative"})
+@EnableJpaRepositories(basePackages = {"org.cooperative"})
 public class CooperativeApplication {
     public static void main(String[] args) {
         SpringApplication.run(CooperativeApplication.class, args);
